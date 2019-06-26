49256273a4b8e8f270bbf2764275c3874bd38d50
Eric Hindman <eric.the.hindman@gmail.com>
2019-06-26 15:57:13 -0400

    fixes

--- react/react.js
+++ react/react.js
@@ -33,25 +33,22 @@ function ToDoApp () {
 		setItems(newState);
 	}
 
-	return (
-		<section>
-			<form onSubmit={handleSubmit}>
-				<input ref={input} autoFocus/>
-				<button type="submit">Add Item</button>
-			</form>
-
-			<ul>
-				{items.map((item, index) => {
-					return <li className={'comp' + item.completed} key={index}>
-						<label>
-							<button className={index} onClick={toggleItem}>√</button>
-							{item.text}
-						</label>
-						<button className={index} onClick={removeItem}>x</button>
-					</li>
-				})}
-			</ul>
-		</section>
-	);
+	return <>
+		<form onSubmit={handleSubmit}>
+			<input ref={input} autoFocus/>
+			<button type="submit">Add Item</button>
+		</form>
+		<ul>
+			{items.map((item, index) => {
+				return <li className={'comp' + item.completed} key={index}>
+					<label>
+						<button className={index} onClick={toggleItem}>√</button>
+						{item.text}
+					</label>
+					<button className={index} onClick={removeItem}>x</button>
+				</li>
+			})}
+		</ul>	
+	</>;	
 }
 ReactDOM.render(<ToDoApp />, document.getElementById('root'));
