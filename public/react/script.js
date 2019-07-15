15ab6cbd867063ae5d262a3bb3bb41b57f75a8a4
Eric Hindman <45405243+erichindman@users.noreply.github.com>
2019-07-15 00:04:56 -0400

    removed server dependencies

--- public/react/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	document.body.appendChild(
-		document.createElement('p')
-	).appendChild(
-		document.createTextNode('Greetings, ' + navigator.vendor + ' browser!')
-	);
-})
